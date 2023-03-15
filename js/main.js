b0c0c437833737adda2365c0e116da97b6101a3b
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-03-15 17:23:23 +0100

    Milestone 3

--- js/main.js
+++ js/main.js
@@ -10,43 +10,32 @@ createApp({
         done: false,
         text: "",
       },
-      todoList: [
-        {
-          done: false,
-          text: "Latte",
-        },
-        {
-          done: false,
-          text: "Pane",
-        },
-        {
-          done: false,
-          text: "uova",
-        },
-        {
-          done: false,
-          text: "carne",
-        },
-        {
-          done: false,
-          text: "Tanto alcol",
-        },
-      ],
+      todoList: [],
     };
   },
   methods: {
     done(index) {
       if (this.todoList[index].done === false) {
         this.todoList[index].done = true;
-        console.log('fatto');
+        console.log("fatto");
       } else if (this.todoList[index].done === true) {
-        this.todoList[index].done === false; 
-        console.log('da fare');
+        this.todoList[index].done === false;
+        console.log("da fare");
       }
-     
     },
-    removeTodo(index){
-        this.todoList.splice(index,1);
-    }
+    removeTodo(index) {
+      this.todoList.splice(index, 1);
+    },
+    addTodo() {
+      if (this.newText.trim() !== "") {
+        let { text, done } = this.todoInfo;
+        text = this.newText;
+        this.todoList.push({
+          text,
+          done,
+        });
+        this.newText = "";
+      }
+    },
   },
 }).mount("#app");
