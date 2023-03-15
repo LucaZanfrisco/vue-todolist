c0a2399d17f43ee7eceb8ab56be9d42ab67d46c5
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-03-15 17:46:21 +0100

    Bonus e aggiunta commenti

--- js/main.js
+++ js/main.js
@@ -14,18 +14,19 @@ createApp({
     };
   },
   methods: {
+    // Metodo che aggiunge la classe todoDone se l'elemento della lista è stato gia fatto
     done(index) {
       if (this.todoList[index].done === false) {
         this.todoList[index].done = true;
-        console.log("fatto");
-      } else if (this.todoList[index].done === true) {
-        this.todoList[index].done === false;
-        console.log("da fare");
+      } else {
+        this.todoList[index].done = false;
       }
     },
+    // Metodo che rimuove l'elemento quando si clicca sull'icona del cestino
     removeTodo(index) {
       this.todoList.splice(index, 1);
     },
+    // Metodo che aggiunge all'array gli oggetti in base a quello che scrivono nell'input per aggiungere ToDo
     addTodo() {
       if (this.newText.trim() !== "") {
         let { text, done } = this.todoInfo;
